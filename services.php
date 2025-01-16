fc76eb9a39f3d4ba2e8fce61c1d3cacfa47f1d39
tristan2603 <gtristan543@gmail.com>
2025-01-16 21:04:01 +0800

    9:03pm 1-16-25 almost done

--- services.php
+++ services.php
@@ -1,3 +1,31 @@
+<?php
+
+include 'MVC/user_routes.php';
+
+    if(!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'ADMIN'){
+        header('location: login_form.php');
+        exit;
+    } else {
+        $userID = $_SESSION['user_id'];
+    }
+
+
+    $user = $control->selectoneuser($userID);
+
+
+    function disp($use){
+        if (!empty($use['user_image'])) {
+            return 'data:image/jpeg;base64,' . base64_encode($use['user_image']);
+        } else {
+            return "images/adduser.png"; // Default image
+        }
+    }
+
+    $service = $servicecontrol->fetchallservice();
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +33,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Services</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .mod {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 600px;
+            height: 500px; 
+            background-color: #FFFFFF;
+            border-radius: 10px;
+            border: 1px solid;
+            display: none;
+        }
+
+    </style>
 </head>
 <body>
     <div class="container-fluid" style="height: 200px; background-color: #131313;">
@@ -47,24 +90,77 @@
     
     <div class="container border border-primary" style="margin-top: 50px;  display: grid;
   grid-template-columns: auto auto auto;">
-        
+        <?php foreach($service as $serve): ?>
         <div class="grid-item" style="border: 1px solid; display: flex; justify-content: center;">
-            <div class="card" style="width: 18rem;">
+            <div class="card tog" id="1234" style="width: 18rem;">
                 <img class="card-img-top" src="images/bg1.png" alt="Card image cap">
                 <div class="card-body">
-                    <p class="card-text">Shiatsu Massage</p>
+                    <p class="card-text"><?php echo $serve['service_name']; ?></p>
                     <p class="card-text">(Dry)</p>
                 </div>
             </div>
+                <div class="mod" id="1234">
+
+                </div>
         </div>
+        <?php endforeach ?>
         <div class="grid-item" style="border: 1px solid; display: flex; justify-content: center;">Item 2</div>
         <div class="grid-item" style="border: 1px solid; display: flex; justify-content: center;">Item 3</div>
     </div>
+
+    
 
    
     
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script>
+        
+        const togglemodal = document.querySelectorAll('.tog');
+        const modd = document.querySelectorAll('.mod');
+        
+        togglemodal.forEach((modal) =>{
+            modal.addEventListener('click', (event)=>{
+                event.stopPropagation();
+                closeall();
+                if(event.target.closest('.tog')){
+                    console.log('catched');
+                    modd.forEach((mods) =>{
+                        console.log(mods.id);
+                    if(event.target.closest('.tog').id === mods.id){
+                        mods.style.display = "block";
+                        }
+                    })
+                }
+            })
+        })
+
+        function closeall(){
+            modd.forEach((mods) =>{
+                mods.style.display = "none";
+            })
+        }
+
+        document.addEventListener('click', ()=>{
+            event.stopPropagation();
+            if(event.target.closest('.mod')){
+                    console.log('succes mod');
+            } else if(event.target.closest('.tog')){
+                modd.forEach((mods) =>{
+                    if(event.target.closest('.tog').id === mods.id){
+                            if(mods.display === "none"){
+                                console.log('okkk');
+                            } else if(mods.display === "block") {
+                                closeall();
+                            }
+                        }
+                    })
+            } else {
+                closeall();
+            }
+        })
+
+    </script>
 </body>
 </html>
