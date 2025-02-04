dc4c077dca9a081d74f7a2814dbc76bae74dd6f2
tristan2603 <gtristan543@gmail.com>
2025-02-04 20:46:03 +0800

    8:45pm 2-4-25

--- middleware/message.php
+++ middleware/message.php
@@ -11,16 +11,19 @@ if(isset($_GET['mess'])){
 
     <style>
         .messmodal{
-            background-color: #ffccf2;
-            border: 1px solid;
+            background-color: #6b3702;
             border-radius: 10px;
-            width: 500px;
+            width: 400px;
             height: 300px;
             position: absolute;
             left: 50%;
             transform: translate(-50%, 20%);
             animation: fadeIn 0.3s ease-out forwards;
             -webkit-animation: fadeIn 0.3s ease-out forwards;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            box-shadow: 1px 1px 10px;
         }
 
         @keyframes fadeIn {
@@ -61,12 +64,20 @@ if(isset($_GET['mess'])){
         }
     </style>
 
-    <div class="messmodal"><?php echo $messval ?></div>
+    <div class="messmodal">
+        <img style="height: 100px;" src="./images/logo.png" alt="./images/logo.png">
+        <div style="border: 1px solid; border-color: #808080; width: 100%; margin-top: 10px;"></div>
+        <div style="border: 1px solid; border-radius: 10px; width: 100%; height: 100%; background-color: white; display: flex; justify-content: center; align-items: center;">
+            <div style="font-size: 30px; font-weight: 700;"><?php echo $messval ?></div>
+        </div>
+        
+    </div>
 
     <script>
         document.addEventListener('click', (event)=>{
             console.log('slooo');
             document.querySelector('.messmodal').style.display = "none";
+            document.querySelector('body').style.overflow = 'visible';
 
             const url = new URL(window.location.href);
             url.searchParams.delete('mess'); // Remove the 'mes' parameter
